d679a44e2e890c80e3005705ada6c0cf9f9b86bc
tiamating <kh.mxt97@gmail.com>
2022-02-18 14:50:52 +0900

    Audioのロードをfalseに変更して実行可能にした

--- TKGEngine/Lib/Application/inc/AudioResourceList.h
+++ TKGEngine/Lib/Application/inc/AudioResourceList.h
@@ -20,7 +20,7 @@ namespace TKGEngine::Audio
 		MAX_NUM_BGM_LIST
 	};
 	static constexpr const int MAX_BGM_LIST = static_cast<int>(BGM_LIST::MAX_NUM_BGM_LIST);
-	static constexpr const bool LOAD_BGM_FILE = true;
+	static constexpr const bool LOAD_BGM_FILE = false;
 	static constexpr const wchar_t* BGM_FILENAMES[MAX_BGM_LIST] =
 	{
 		L"./Asset/Audio/BGM/test.wav"
@@ -50,7 +50,7 @@ namespace TKGEngine::Audio
 		MAX_NUM_SE_LIST
 	};
 	static constexpr const int MAX_SE_LIST = static_cast<int>(SE_LIST::MAX_NUM_SE_LIST);
-	static constexpr const bool LOAD_SE_FILE = true;
+	static constexpr const bool LOAD_SE_FILE = false;
 	static constexpr const wchar_t* XWB_FILENAME = L"./Asset/Audio/SE/test.xwb";
 	static constexpr const int WAVE_SOUND_MAX = 16;
 	static constexpr float SE_VOLUMES[MAX_SE_LIST] =
